3af4737cd7c5686c3adb938edfcf42a24381b6cd
KEOPHIPHATH Arinhasvath <9295@holbertonstudents.com>
2024-07-24 10:13:45 +0200

    allfiles

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+
+
+
+#endif /* MAIN_H */
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
