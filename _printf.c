b220eb2e2618c11e1a3d785f2313bbddb90c8741
Arinhakeo <9295@holbertonstudents.com>
2024-07-24 09:19:53 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,41 +1,46 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
-    int count = 0;
-    const char *p = format;
-    while (*p != '\0') 
-    {
-        if (*p == '%') 
-        {
-            p++;
-            if (*p == 'c') 
-            {
-                char ch = (char) va_arg(args, int);
-                putchar(ch);
-                count++;
-            } else if (*p == 's') 
-            {
-                char* str = va_arg(args, char*);
-                while (*str != '\0') 
-                {
-                    putchar(*str);
-                    str++;
-                    count++;
-                }
-            }
-        } 
-        else 
-        {
-            putchar(*p);
-            count++;
-        }
-        p++;
-    }
-    va_end(args);
-    return count;
+	va_list args;
+	const char *p;
+	int count;
+
+	va_start(args, format);
+	count = 0;
+	p = format;
+
+	while (*p != '\0')
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == 'c')
+			{
+				char car = (char) va_arg(args, int);
+				putchar(car);
+				count++;
+			}
+			else if (*p == 's')
+			{
+				char* str = va_arg(args, char*);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					count++;
+				}
+			}
+		}
+		else
+		{
+			putchar(*p);
+			count++;
+		}
+		p++;
+	}
+	va_end(args);
+	return count;
 }
